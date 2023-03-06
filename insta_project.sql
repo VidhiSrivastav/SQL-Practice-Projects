da7998409152fe2ab40e3fa6d07d21408837ea90
Vids <122469579+Vids333@users.noreply.github.com>
2023-03-06 18:23:29 +0530

    Add files via upload

--- insta_project.sql
+++ insta_project.sql
@@ -47,3 +47,72 @@ FROM (SELECT users.username,COUNT(photos.image_url)AS  total_posts_per_user
 FROM users
 JOIN photos ON users.id = photos.user_id
 GROUP BY users.id) AS user_posts;
+
+#total numbers of users who have posted at least one time *
+
+SELECT COUNT(DISTINCT(users.id)) AS total_users_posted
+FROM users
+join photos ON users.id = photos.user_id;
+
+#A brand wants to know which hashtags to use in a post
+#What are the top 5 most commonly used hashtags?
+
+SELECT tag_name, COUNT(tag_name) AS total
+FROM tags
+JOIN photo_tags ON tags.id = photo_tags.tag_id
+GROUP BY tags.id
+ORDER BY total DESC;
+
+#We have a small problem with bots on our site...
+#Find users who have liked every single photo on the site
+
+SELECT users.id, username, COUNT(users.id)AS total_likes 
+FROM users
+JOIN likes ON users.id = likes.user_id
+GROUP BY users.id
+HAVING total_likes = (SELECT COUNT(*) FROM photos);
+
+#We also have a problem with celebrities
+#Find users who have never commented on a photo
+
+SELECT count(*) 
+FROM
+(SELECT username, comment_text
+FROM users
+LEFT JOIN comments ON users.id = comments.user_id
+GROUP BY users.id
+HAVING comment_text IS NULL) AS total_number_of_users_without_comments;
+
+
+#Find the percentage of our users who have either never commented on a photo or have commented on every photo*
+
+SELECT 
+tableA.total_A AS number_of_users_never_commented,
+(tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS "%",
+tableB.total_B AS number_of_users_liked_every_photo,
+(tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS "%"
+
+FROM 
+(
+SELECT COUNT(*) AS total_A 
+FROM
+(SELECT username, comment_text
+FROM users
+LEFT JOIN comments ON users.id = comments.user_id
+GROUP BY users.id
+HAVING comment_text IS NULL ) AS total_numbers_without_comments
+) 
+AS tableA
+
+JOIN
+(
+SELECT COUNT(*) AS total_B
+FROM 
+(
+SELECT users.id, username,COUNT(users.id) AS total_likes_by_user
+FROM users
+JOIN likes ON users.id  = likes.user_id
+GROUP BY users.id
+HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos)) AS total_nuber_users_likes_every_photo
+)
+AS tableB;
